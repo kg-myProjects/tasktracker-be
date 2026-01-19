020a1700d26c74ff53ce77915b764b75b7cff5de
maximheit1989-arch <maxim.heit1989@gmail.com>
2026-01-19 15:49:54 +0100

    #FE-220 Added Button "Forgot password?"

--- src/main/java/de/upteams/tasktracker/security/entities/PasswordResetToken.java
+++ src/main/java/de/upteams/tasktracker/security/entities/PasswordResetToken.java
@@ -1,23 +1,23 @@
 package de.upteams.tasktracker.security.entities;
 
 import de.upteams.tasktracker.user.entity.AppUser;
-import jakarta.persistence.*;
+import de.upteams.tasktracker.utils.BaseEntity;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import java.time.LocalDateTime;
-import java.util.UUID;
 
 @Entity
+@Table(name = "password_reset_tokens")
 @Getter
 @Setter
 @NoArgsConstructor
-public class PasswordResetToken {
-
-    @Id
-    @GeneratedValue
-    private UUID id;
+public class PasswordResetToken extends BaseEntity {
 
     @Column(nullable = false, unique = true)
     private String token;
@@ -35,5 +35,10 @@ public class PasswordResetToken {
         this.token = token;
         this.user = user;
         this.expiresAt = expiresAt;
+    }
+
+    @Override
+    public String toString() {
+        return "PasswordResetToken{id=" + id + "}";
     }
 }
