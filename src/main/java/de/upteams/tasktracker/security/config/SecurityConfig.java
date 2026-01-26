ef8f3c7ea9eda58c4649f17b36917602fa51ae00
maximheit1989-arch <maxim.heit1989@gmail.com>
2026-01-26 15:38:06 +0100

    #FE-220 Added Button "Forgot password?" Fixed SecurityConfig-cors

--- src/main/java/de/upteams/tasktracker/security/config/SecurityConfig.java
+++ src/main/java/de/upteams/tasktracker/security/config/SecurityConfig.java
@@ -68,7 +68,7 @@ public class SecurityConfig {
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
                 .csrf(AbstractHttpConfigurer::disable)
-                .cors(AbstractHttpConfigurer::disable)
+                .cors(cors -> {})
                 .sessionManagement(sm -> sm.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authenticationProvider(authenticationProvider())
                   .authorizeHttpRequests(auth -> auth
