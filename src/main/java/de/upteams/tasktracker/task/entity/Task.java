e5b27eed436af061247b311db9b8ac738614e903
Dushyna <olgavnv777@gmail.com>
2026-03-12 13:26:54 +0100

    #360 Add Data of task creation and modification(Date in format Instant)

--- src/main/java/de/upteams/tasktracker/task/entity/Task.java
+++ src/main/java/de/upteams/tasktracker/task/entity/Task.java
@@ -15,7 +15,7 @@ import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 
-import java.time.LocalDateTime;
+import java.time.Instant;
 import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.List;
@@ -73,7 +73,7 @@ public class Task extends BaseEntity {
     private List<ChecklistItem> checklist = new ArrayList<>();
 
     @Column(name = "due_date")
-    private LocalDateTime dueDate;
+    private Instant dueDate;
 
     @OneToMany(mappedBy = "task", cascade = CascadeType.ALL, orphanRemoval = true)
     @OrderBy("createdAt ASC")
@@ -85,20 +85,20 @@ public class Task extends BaseEntity {
     private List<Comment> comments = new ArrayList<>();
 
     @Column(name = "created_at", nullable = false, updatable = false)
-    private LocalDateTime createdAt;
+    private Instant createdAt;
 
     @Column(name = "updated_at")
-    private LocalDateTime updatedAt;
+    private Instant  updatedAt;
 
     @PrePersist
     protected void onCreate() {
-        createdAt = LocalDateTime.now();
-        updatedAt = LocalDateTime.now();
+        createdAt = Instant .now();
+        updatedAt = Instant .now();
     }
 
     @PreUpdate
     protected void onUpdate() {
-        updatedAt = LocalDateTime.now();
+        updatedAt = Instant .now();
     }
 
 
